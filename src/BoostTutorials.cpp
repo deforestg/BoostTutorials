e5e1cf03ac9bf8e19e038c55e00e54f48e99c724
Gabriel de Forest <deforestg@gmail.com>
2014-08-12 20:21:16 -0400

    Moving the timer tutorials into a subfolder in prep for sockets

--- src/BoostTutorials.cpp
+++ src/BoostTutorials.cpp
@@ -11,15 +11,16 @@
 #include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
-#include "tutorials/include/TutorialOne.h"
-#include "tutorials/include/TutorialTwo.h"
-#include "tutorials/include/TutorialThree.h"
-#include "tutorials/include/TutorialFour.h"
-#include "tutorials/include/TutorialFive.h"
+#include "tutorials/timers/include/TutorialOne.h"
+#include "tutorials/timers/include/TutorialTwo.h"
+#include "tutorials/timers/include/TutorialThree.h"
+#include "tutorials/timers/include/TutorialFour.h"
+#include "tutorials/timers/include/TutorialFive.h"
 
 using namespace std;
 
-int main() {
+void runTheTimers()
+{
 	TutorialOne *T1 = new TutorialOne();
 	T1->Execute();
 
@@ -29,11 +30,15 @@ int main() {
 	TutorialThree *T3 = new TutorialThree(2, 1);
 	T3->Execute();
 
-	TutorialFour *T4 = new TutorialFour(3, 1);
+	TutorialFour *T4 = new TutorialFour(2, 1);
 	T4->Execute();
 
 	TutorialFive *T5 = new TutorialFive(20, 2, 4);
 	T5->Execute();
+}
 
+int main()
+{
+	runTheTimers();
 	return 0;
 }
